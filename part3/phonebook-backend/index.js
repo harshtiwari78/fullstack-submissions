aec785915fa8f4134040cb2328e772ce4698563d
Harsh Tiwari <harshtiwari787898@gmail.com>
2025-10-29 15:20:02 +0530

    ex 3.17~~

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -8,8 +8,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
-app.use(morgan('tiny'))
-
 morgan.token('body', (req, res) => {
     if (req.body) {
         return JSON.stringify(req.body);
